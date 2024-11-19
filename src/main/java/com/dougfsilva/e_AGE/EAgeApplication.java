2a49812f1f56b62ab5110391abbd8243518a2112
Douglas Ferreira da Silva <doug.ferreira.dfs@gmail.com>
2024-11-18 22:42:13 -0300

    Projeto iniciado

---
+++ src/main/java/com/dougfsilva/e_AGE/EAgeApplication.java
@@ -0,0 +1,13 @@
+package com.dougfsilva.e_AGE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dougfsilva/e_AGE/EAgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougfsilva.e_AGE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
