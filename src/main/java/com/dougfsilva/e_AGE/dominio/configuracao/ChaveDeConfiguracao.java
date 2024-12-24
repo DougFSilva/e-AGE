8df810b3bcd356c02d64fb4b9fba0e108f16f6df
Douglas Ferreira da Silva <doug.ferreira.dfs@gmail.com>
2024-12-24 08:09:54 -0300

    Criado enum ChaveDeConfiguracao e classe Configuracao para representar as configurações do sistema salvos no banco de dados

---
+++ src/main/java/com/dougfsilva/e_AGE/dominio/configuracao/ChaveDeConfiguracao.java
@@ -0,0 +1,42 @@
+package com.dougfsilva.e_AGE.dominio.configuracao;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum ChaveDeConfiguracao {
+
+	NOME_ESCOLA(null), EMAIL_DE_CONTATO(null), TELEFONE_DE_CONTATO(null),
+	ENVIAR_EMAIL_PARA_FUNCIONARIO_AO_CRIAR_USUARIO(List.of("SIM", "NÃO")),
+	ENVIAR_EMAIL_PARA_ESTUDANTE_AO_CRIAR_USUARIO(List.of("SIM", "NÃO")),
+	ENVIAR_EMAIL_AO_CRIAR_OCORRENCIA(List.of("SIM", "NÃO")), ENVIAR_EMAIL_AO_EXCLUIR_OCORRENCIA(List.of("SIM", "NÃO")),
+	ENVIAR_EMAIL_AO_ATUALIZAR_OCORRENCIA(List.of("SIM", "NÃO")),
+	ENVIAR_EMAIL_AO_ENCERRAR_OCORRENCIA(List.of("SIM", "NÃO")),
+	ENVIAR_MENSAGEM_PARA_FUNCIONARIO_AO_CRIAR_USUARIO(List.of("SIM", "NÃO")),
+	ENVIAR_MENSAGEM_PARA_ESTUDANTE_AO_CRIAR_USUARIO(List.of("SIM", "NÃO")),
+	ENVIAR_MENSAGEM_AO_CRIAR_OCORRENCIA(List.of("SIM", "NÃO")),
+	ENVIAR_MENSAGEM_AO_EXCLUIR_OCORRENCIA(List.of("SIM", "NÃO")),
+	ENVIAR_MENSAGEM_AO_ATUALIZAR_OCORRENCIA(List.of("SIM", "NÃO")),
+	ENVIAR_MENSAGEM_AO_ENCERRAR_OCORRENCIA(List.of("SIM", "NÃO"));
+
+	private final List<String> valoresPermitidos;
+
+	ChaveDeConfiguracao(List<String> valoresPermitidos) {
+		this.valoresPermitidos = valoresPermitidos;
+	}
+
+	public List<String> getValoresPermitidos() {
+		return valoresPermitidos;
+	}
+
+	public static ChaveDeConfiguracao deNomeChave(String nomeChave) {
+		return Arrays.stream(values()).filter(chave -> chave.name().equalsIgnoreCase(nomeChave)).findFirst()
+				.orElseThrow(() -> new IllegalArgumentException("Chave de configuração inválida: " + nomeChave));
+	}
+
+	public void validarValor(String valor) {
+		if (valoresPermitidos != null && !valoresPermitidos.contains(valor)) {
+			throw new IllegalArgumentException("Valor de configuração inválido: " + valor);
+		}
+	}
+
+}
---
+++ src/main/java/com/dougfsilva/e_AGE/dominio/configuracao/Configuracao.java
@@ -0,0 +1,38 @@
+package com.dougfsilva.e_AGE.dominio.configuracao;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(of = { "chave" })
+@ToString
+
+public class Configuracao {
+
+	private String ID;
+	private ChaveDeConfiguracao chave;
+	private String valor;
+
+	public Configuracao(ChaveDeConfiguracao chave, String valor) {
+		this.chave = chave;
+		this.chave.validarValor(valor);
+		this.valor = valor;
+	}
+
+	public void setID(String ID) {
+		this.ID = ID;
+	}
+
+	public void setChave(ChaveDeConfiguracao chave) {
+		this.chave = chave;
+	}
+
+	public void setValor(String valor) {
+		this.chave.validarValor(valor);
+		this.valor = valor;
+	}
+
+}
