c62082570d1c6a296e5388f07c10cbaae7102095
Douglas Ferreira da Silva <doug.ferreira.dfs@gmail.com>
2024-12-26 08:18:17 -0300

    Criado usecase ExcluiMOdulo

---
+++ src/main/java/com/dougfsilva/e_AGE/aplicacao/casosdeuso/curso/modulo/ExcluiModulo.java
@@ -0,0 +1,33 @@
+package com.dougfsilva.e_AGE.aplicacao.casosdeuso.curso.modulo;
+
+import com.dougfsilva.e_AGE.aplicacao.casosdeuso.utilidades.LogPadrao;
+import com.dougfsilva.e_AGE.dominio.curso.modulo.Modulo;
+import com.dougfsilva.e_AGE.dominio.curso.modulo.ModuloRepository;
+import com.dougfsilva.e_AGE.dominio.exception.ErroDeOperacaoComModuloException;
+import com.dougfsilva.e_AGE.dominio.exception.ObjetoNaoEncontradoException;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ExcluiModulo {
+
+	private final ModuloRepository repository;
+	private final LogPadrao log;
+	
+	public void excluirPeloID(String ID) {
+		try {
+			Modulo modulo = repository.buscarPeloIDOuThrow(ID);
+			repository.excluir(modulo);
+			log.info(String.format("Excluido módulo %s do curso %s", modulo.getCodigo(), modulo.getCurso()));
+		} catch (ObjetoNaoEncontradoException e) {
+			String mensagem = String.format("Erro ao excluir módulo com ID %s : %s", ID, e.getMessage());
+			log.warn(mensagem, e);
+			throw new ErroDeOperacaoComModuloException(mensagem, e);
+		} catch (Exception e) {
+			String mensagem = String.format("Erro inesperado ao excluir módulo com ID %s : %s", ID, e.getMessage());
+			log.error(mensagem, e);
+			throw new ErroDeOperacaoComModuloException(mensagem, e);
+		}
+	}
+	
+}
